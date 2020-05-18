96f91afa09bd212115306e243b0c3bb581f74be8
AleksBobic <bobicaleks@yahoo.com>
2020-05-18 19:44:14 +0200

    first commit udemy course app

---
+++ src/user/pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import UsersList from '../components/UsersLists'
+
+const Users = () => {
+   const USERS = [{id: '1', name: 'Salezy', image: 'https://miro.medium.com/max/11344/1*32h8ts3A-7XNr6A4Js87ng.jpeg', places: 1}]
+
+  return <UsersList items={USERS}/>
+}
+
+export default Users
