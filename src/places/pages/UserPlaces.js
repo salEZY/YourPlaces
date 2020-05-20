b3fce7cb4638179457939cc2112e6e67d9ae425d
AleksBobic <bobicaleks@yahoo.com>
2020-05-20 22:54:24 +0200

    add button component to userItem

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList'
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Beograd',
+    description: 'NAJJACI GRAD',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Beograd_night.png',
+    address: 'Kneza Milosa 2',
+    location: {
+      lat: 44.8149028,
+      lng: 20.1424149
+    },
+    creator: 'p1'
+  },
+  {
+    id: 'p2',
+    title: 'Beograd',
+    description: 'GRAD BEO',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Beograd_night.png',
+    address: 'Kneza Milosa 2',
+    location: {
+      lat: 44.8149028,
+      lng: 20.1424149
+    },
+    creator: 'p2'
+  }
+]
+
+const UserPlaces = () => {
+  const userId = useParams().userID
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+  return <PlaceList items={loadedPlaces}/>
+}
+
+export default UserPlaces
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,7 +3,7 @@ import React from 'react'
 import UsersList from '../components/UsersLists'
 
 const Users = () => {
-   const USERS = [{id: '1', name: 'Salezy', image: 'https://miro.medium.com/max/11344/1*32h8ts3A-7XNr6A4Js87ng.jpeg', places: 1}]
+   const USERS = [{id: 'p1', name: 'Salezy', image: 'https://miro.medium.com/max/11344/1*32h8ts3A-7XNr6A4Js87ng.jpeg', places: 1}]
 
   return <UsersList items={USERS}/>
 }
