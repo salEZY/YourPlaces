7f215769c5d767597bc260322f41cd0928db6018
AleksBobic <bobicaleks@yahoo.com>
2020-07-24 22:26:53 +0200

    start get users route

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersLists'
+import UsersList from "../components/UsersLists";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-   const USERS = [{id: 'u1', name: 'Salezy', image: 'https://miro.medium.com/max/11344/1*32h8ts3A-7XNr6A4Js87ng.jpeg', places: 1},
-   {id: 'u2', name: 'Chika Salezy', image: 'https://miro.medium.com/max/11344/1*32h8ts3A-7XNr6A4Js87ng.jpeg', places: 1}]
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS}/>
-}
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users/");
 
-export default Users
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
